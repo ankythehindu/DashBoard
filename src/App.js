979bb41a9edcd920bec6bffb22823ae7c7fe58cc
ankitthehindu <ankitmaheshwari7099@gmail.com>
2024-09-10 09:37:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css'
-
+import Sidebar from './Component/Sidebar/Sidebar.jsx';
+import MainDash from './Component/MainDash/MainDash.jsx'
+import RightSide from './Component/RightSide/RightSide.jsx';
 function App() {
   return (
     <div className="App">
-        Subscribe Zainkeepscode
+       <div className='AppGlass'>
+       <Sidebar/>
+        <MainDash/>
+        <RightSide/>
+      
+       </div>
     </div>
   );
 }
